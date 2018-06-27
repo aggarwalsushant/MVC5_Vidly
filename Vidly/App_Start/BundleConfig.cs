0e908704bfd00472ed00d636af4d888f38b89245
Sushant Aggarwal <sushant.aggarwal@ihsmarkit.com>
2018-06-27 07:15:40 +0100

    add jquery datatables nuget package and use it on the customers main page > customer list table. and a little playaround with the css to sort out some errors. Although, images for sort desc on the datatable plugin are still NOT coming up good. Left it as it is, as of now.

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -8,8 +8,14 @@ namespace Vidly
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/bootbox.js",
+                        "~/Scripts/DataTables/jquery.datatables.js",
+                        "~/Scripts/DataTables/datatables.bootstrap.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,8 +31,11 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       //"~/Content/bootstrap.css",
+                      "~/Content/DataTables/css/*.css",
+                      //"~/Content/DataTables/css/datatables.bootstrap.css",
                       "~/Content/bootstrap-lumen.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css" // This is put at last to override any classes present in the above files.
+                      ));
         }
     }
 }
