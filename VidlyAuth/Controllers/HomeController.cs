3041ab058477ce884ed2b154aa97076917094971
Sushant Aggarwal <sushant.aggarwal@ihsmarkit.com>
2018-06-27 12:54:36 +0100

    starting a new orphan branch for Authentication and Authorisation section of the ASP.Net MVC 5 Course. It's a default project created by VS and no integration has been done with the Vidly App. Although, the course video shows the Vidly App with auth things already done. When and how he did it....Not shown!

---
+++ VidlyAuth/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VidlyAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Without the AllowAnonymous tag, this works as well.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Anybody can access this page.
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        // Authorize tag ensures that user is logged in before viewing this
+        // else it is shown login page.
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
